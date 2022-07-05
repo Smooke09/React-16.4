ecd4a018c68aa5055f38581525dba8d42fc5ab17
Smooke09 <pedrolucas1014zz@gmail.com>
2022-07-05 11:48:30 -0300

    Components multiple

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // import Primary from "./Components/Primary";
-import BomDia from "./Components/BomDia";
+// import BomDia from "./Components/BomDia";
+
+import Multi from "./Components/Multiplos";
 
 ReactDOM.render(
-  <BomDia text="Teste" value={true} />,
+  <div>
+    <Multi.BoaTarde name="Pedro" />
+    <Multi.BoaNoite name="Gabriela" />
+  </div>,
   document.getElementById("root")
 );
